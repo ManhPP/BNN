598f9ab00f06e589552181b2b18a90567d79717e
manhpp <phumanh1998@gmail.com>
2021-07-18 11:33:36 +0700

    update

--- profiler.py
+++ profiler.py
@@ -1,13 +1,20 @@
 import torch
 from torch.profiler import profile, record_function, ProfilerActivity
 from torchvision import datasets, transforms
+from torchsummary import summary
+
+import tracemalloc
 
 from src.model.binary_cnn import BinaryCNN
 from src.model.cnn import CNN
 from src.model.fc import BinaryFC, FC
 
-model = FC()
+# model = BinaryFC()
+# model = BinaryCNN()
+# model = FC()
+model = CNN()
 
+summary(model, (28, 28, 1))
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 checkpoint = torch.load("checkpoint/" + model._get_name() + ".pth", map_location=device)
 state_dict = checkpoint['net']
@@ -28,6 +35,15 @@ inputs, targets = dataiter.next()
 
 with profile(activities=[ProfilerActivity.CPU], profile_memory=True, record_shapes=True) as prof:
     with record_function("model_inference"):
+        tracemalloc.start()
         model(inputs)
+        current, peak = tracemalloc.get_traced_memory()
+        tracemalloc.stop()
 
 print(prof.key_averages().table())
+print(f"{current:0.2f}, {peak:0.2f}")
+
+#   14403.00, 29252.00
+#   14483.00, 29331.00
+#   463114.00, 476873.00
+#   462784.00, 476743.00
